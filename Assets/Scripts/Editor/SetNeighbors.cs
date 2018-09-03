000db78e23ecdabf4e8e19d67c76db23f4a24c5f
avalon <yuanqi.shan@aalto.fi>
2018-09-04 00:34:00 +0300

    Added Editor script SetNeighbors that sets all node neighbors

---
+++ Assets/Scripts/Editor/SetNeighbors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SetNeighbors : MonoBehaviour {
+
+    [MenuItem("Node/Set Neighbors")]
+    public static void ShowWindow()
+    {
+        var nodes = FindObjectsOfType<Node>();
+
+        for (int i = 0; i < nodes.Length; i++)
+            nodes[i].CheckNeighbors();
+    }
+}
--- Assets/Scripts/Game/Node.cs
+++ Assets/Scripts/Game/Node.cs
@@ -6,14 +6,19 @@ public class Node : MonoBehaviour {
 
     public List<Node> neighbors;
     public Color gizmoColor;
+    private static readonly Color neighborColor = Color.magenta;
 
     private void Start()
     {
         var sprite = GetComponent<SpriteRenderer>();
-        if (sprite)
+        if (sprite && Application.isPlaying)
             sprite.enabled = false;
+        CheckNeighbors();
+    }
 
-        for(int i = 0; i < neighbors.Count; i++)
+    public void CheckNeighbors()
+    {
+        for (int i = 0; i < neighbors.Count; i++)
         {
             var n = neighbors[i];
             if (!n.neighbors.Contains(this))
@@ -23,7 +28,8 @@ public class Node : MonoBehaviour {
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = gizmoColor;
-        Gizmos.DrawSphere(transform.position, 0.5f);
+        Gizmos.color = neighborColor;
+        for (int i = 0; i < neighbors.Count; i++)
+            Gizmos.DrawLine(transform.position, neighbors[i].transform.position);
     }
 }
