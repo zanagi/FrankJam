02e65b0080b607a595e7287c7d71cf56edd81da1
avalon <yuanqi.shan@aalto.fi>
2018-09-04 12:06:28 +0300

    Fix frank and nodes 2D physics settings

---
+++ Assets/Scripts/Game/Shop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    public float visibility = 25f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var frank = other.GetComponent<Frank>();
+
+        if(frank)
+        {
+            Debug.Log("Frank in shop area");
+        }
+    }
+}
