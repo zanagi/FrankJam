b53eacffba344dbff37e7e8499ef9181e1524c74
avalon <yuanqi.shan@aalto.fi>
2018-09-06 15:42:58 +0300

    Bomb and explosioon mechanics

--- Assets/Scripts/Game/Bomb.cs
+++ Assets/Scripts/Game/Bomb.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour {
 
-    public float time, shaking = 0.1f;
+    public float time, shaking = 0.1f, explosionTime = 1.5f, explosionScale = 2.0f;
+    public GameObject explosion;
     private Vector3 startPos;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         startPos = transform.localPosition;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        explosion.transform.localScale = Vector3.zero;
+        explosion.SetActive(false);
     }
 
     void Update ()
     {
-        if (!GameManager.Instance.IsIdle)
+        if (!GameManager.Instance.IsIdle && time > 0)
             return;
         time -= Time.deltaTime;
         transform.localPosition = startPos + 
@@ -26,6 +31,21 @@ public class Bomb : MonoBehaviour {
 
     private void Explode()
     {
+        spriteRenderer.enabled = false;
+        StartCoroutine(AnimateExplosion());
+    }
+
+    private IEnumerator AnimateExplosion()
+    {
+        var t = 0.0f;
+        explosion.SetActive(true);
+        while(t < explosionTime)
+        {
+            t += Time.deltaTime;
+            explosion.transform.localScale =
+                Vector3.Lerp(Vector3.zero, Vector3.one * explosionScale, t / explosionTime);
+            yield return null;
+        }
         Destroy(gameObject);
     }
 }
