aba482c22983c544bcabf4beaa317b70104fee1a
zanagi <zanagiys@gmail.com>
2018-09-06 22:39:23 +0300

    Assassin mechanics

--- Assets/Assassin.cs
+++ Assets/Assassin.cs
@@ -2,38 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Assassin : MonoBehaviour {
+public class Assassin : SelectableObject {
 
 	public float speed = 1, animatorSpeed = 0.6f;
 	public Node targetNode;
-	public SpriteRenderer spriteRenderer;
-	public float animModifier = 3.0f;
+	public SpriteRenderer spriteRenderer, background;
+	public Color normalColor = Color.yellow * 0.5f, hiredColor = Color.red * 0.5f;
+	public float animModifier = 3.0f, hiringTime;
+	public bool hired;
+	public int price = 2000, priceIncrease = 1000;
 
 	private Node previousNode;
 	private List<Node> visitedNodes;
 	private Animator animator;
-	private static readonly string frontBool = "Front";
+	private static readonly string frontBool = "Front", killSfx = "DeathAssassin";
 
 	// Use this for initialization
-	void Start()
+	protected override void Start()
 	{
+		base.Start();
 		visitedNodes = new List<Node>();
 		transform.position = targetNode.transform.position;
 		SetNextNode();
+		background.color = normalColor;
 
 		// Animator delay
 		animator = GetComponentInChildren<Animator>();
 		animator.speed = animatorSpeed;
 		animator.Update(Random.Range(0.0f, 1.0f));
+
+		// Window effects
+		windowInstance.actionButton1.onClick.AddListener(() => Hire());
+		SetPriceText();
+	}
+
+	private void SetPriceText()
+	{
+		windowInstance.price.text = string.Format("{0} MK", price);
+	}
+
+	private void Hire()
+	{
+		if(GameManager.Instance.SpendMoney(price))
+		{
+			background.color = hiredColor;
+			hired = true;
+
+			// Deselect window after buying event
+			OnDeselect();
+
+			GameManager.Instance.notificationManager.
+				ShowNotification("Assassin hired!");
+		}
 	}
 
 	// Update is called once per frame
-	void Update()
+	protected override void Update()
 	{
 		if (!GameManager.Instance.IsIdle)
 		{
 			return;
 		}
+		base.Update();
+
 		if (targetNode)
 		{
 			var delta = targetNode.transform.position - transform.position;
@@ -96,11 +127,21 @@ public class Assassin : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		var frank = collision.GetComponent<Frank>();
-
-		if(frank)
+		if (hired)
 		{
-			Debug.Log("COntacfkljldk");
+			var frank = collision.GetComponent<Frank>();
+
+			if (frank)
+			{
+				GameManager.Instance.notificationManager.
+					ShowNotification("Frank killed by an assassin!");
+				SFXManager.Instance.PlaySFX(killSfx);
+				frank.Die();
+				hired = false;
+				background.color = normalColor;
+				price += priceIncrease;
+				SetPriceText();
+			}
 		}
 	}
 }
