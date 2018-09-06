06bfcfcccce48b0f880865a3151be2f28d851d38
zanagi <zanagiys@gmail.com>
2018-09-06 23:51:37 +0300

    Optional building windows

---
+++ Assets/SelectableBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableBuilding : SelectableObject {
+
+	public string buildingName;
+	[TextArea]
+	public string description;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+
+		windowInstance.title.text = buildingName.ToUpper();
+		windowInstance.price.gameObject.SetActive(false);
+		windowInstance.description.transform.localPosition
+			= windowInstance.price.transform.localPosition;
+		windowInstance.description.text = description.ToUpper();
+		windowInstance.actionButton1.gameObject.SetActive(false);
+	}
+}
