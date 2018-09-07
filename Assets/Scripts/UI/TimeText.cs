47dd02f7a6737849df779cbc904c60c471fb6593
avalon <yuanqi.shan@aalto.fi>
2018-09-07 13:30:12 +0300

    Added clear time ui to win screen

--- Assets/Scripts/UI/TimeText.cs
+++ Assets/Scripts/UI/TimeText.cs
@@ -5,20 +5,27 @@ using UnityEngine.UI;
 
 public class TimeText : MonoBehaviour {
 
+    public string front;
 	private Text text;
 
 	// Use this for initialization
 	void Start () {
 		text = GetComponent<Text>();
+        SetText();
 	}
 	
 	void Update ()
 	{
-		var totalSeconds = (int)GameManager.Instance.time;
-		var totalHours = totalSeconds / 60 / 60;
-		totalSeconds -= totalHours * 60 * 60;
-		var totalMinutes = totalSeconds / 60;
-		totalSeconds -= totalMinutes * 60;
-		text.text = string.Format("{0}:{1}:{2}", totalHours.ToString("00"), totalMinutes.ToString("00"), totalSeconds.ToString("00"));
-	}
+        SetText();
+    }
+
+    private void SetText()
+    {
+        var totalSeconds = (int)GameManager.Instance.time;
+        var totalHours = totalSeconds / 60 / 60;
+        totalSeconds -= totalHours * 60 * 60;
+        var totalMinutes = totalSeconds / 60;
+        totalSeconds -= totalMinutes * 60;
+        text.text = front + string.Format("{0}:{1}:{2}", totalHours.ToString("00"), totalMinutes.ToString("00"), totalSeconds.ToString("00"));
+    }
 }
