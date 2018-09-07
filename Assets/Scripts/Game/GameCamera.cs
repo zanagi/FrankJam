64d5c5a4d73b320576e41eef510d236cd23eb22d
avalon <yuanqi.shan@aalto.fi>
2018-09-07 12:36:03 +0300

    camera edge

--- Assets/Scripts/Game/GameCamera.cs
+++ Assets/Scripts/Game/GameCamera.cs
@@ -7,8 +7,6 @@ public class GameCamera : MonoBehaviour {
     public Camera Camera { get; private set; }
     public float speed = 1, zoomSpeed = 1;
     public int minZoom = 5, maxZoom = 8;
-    private bool overEdge;
-    private Vector3 overEdgeDir;
 
 	// Use this for initialization
 	void Start () {
@@ -37,39 +35,19 @@ public class GameCamera : MonoBehaviour {
 
     private void HandleMove(float time)
     {
-        var deltaX = Input.GetAxis("Horizontal");
-        var deltaY = Input.GetAxis("Vertical");
+        var delta = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         var mouseViewPos = Camera.ScreenToViewportPoint(Input.mousePosition);
         var mouseDelta = GameManager.Instance.MouseDelta;
-        /*
-        if (mouseViewPos.x > 0.99f || mouseViewPos.x < 0.01f
-            || mouseViewPos.y > 0.99f || mouseViewPos.y < 0.01f)
-        {
-            if (!overEdge)
-            {
-                overEdgeDir = mouseDelta.normalized;
-                overEdge = true;
-            } 
-            else
-            {
-                if(mouseDelta.x != 0 && mouseDelta.y != 0)
-                    overEdgeDir = mouseDelta.normalized;
-            }
-            if(overEdgeDir.x > 0)
-                deltaX = Mathf.Max(deltaX, overEdgeDir.x);
-            else
-                deltaX = Mathf.Min(deltaX, overEdgeDir.x);
-            if(overEdgeDir.y > 0)
-                deltaY = Mathf.Max(deltaY, overEdgeDir.y);
-            else
-                deltaY = Mathf.Min(deltaY, overEdgeDir.y);
-        }
-        else
-        {
-            overEdge = false;
-        }
-        */
-        transform.position += new Vector3(deltaX, deltaY) * speed * time;
+        if (mouseViewPos.x > 0.99f)
+            delta.x = 1;
+        if (mouseViewPos.x < 0.01f)
+            delta.x = -1;
+        if (mouseViewPos.y > 0.99f)
+            delta.y = 1;
+        if (mouseViewPos.y < 0.01f)
+            delta.y = -1;
+        transform.position += delta * speed * time;
+
         CheckBounds();
     }
 
